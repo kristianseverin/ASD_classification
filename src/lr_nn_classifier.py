c11bffa508f4aaff3a12e88264ee0ec6f93c80a5
laerkebraedder <l-hejn@live.dk>
2022-12-14 09:29:04 +0100

    bert finetuning

--- src/lr_nn_classifier.py
+++ src/lr_nn_classifier.py
@@ -19,6 +19,7 @@ from sklearn.model_selection import train_test_split
 from datasets import Dataset
 from sklearn.metrics import classification_report
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer #xxx
 
 # plotting tools
 import matplotlib.pyplot as plt
@@ -42,20 +43,20 @@ class LogReg(nn.Module):
 class NeuralNetwork(nn.Module):
     def __init__(self, n_input_features):
         super().__init__()
-        self.linear1 = nn.Linear(n_input_features, 100)
-        self.linear2 = nn.Linear(100, 10)
-        self.linear3 = nn.Linear(10, 1)
+        self.linear1 = nn.Linear(n_input_features, 20)
+        self.linear2 = nn.Linear(20, 1)
+        #self.linear3 = nn.Linear(10, 1)
 
     def forward(self, x):
-        leaky_relu = nn.LeakyReLU(0.2)
+        leaky_relu = nn.LeakyReLU(0.2) #xxx Ross had 0.2
         # Linear -> ReLU
         x = self.linear1(x)
         x = leaky_relu(x)
         # Linear -> ReLU
         x = self.linear2(x)
-        x = leaky_relu(x)
+        #x = leaky_relu(x)
         # Linear -> Sigmoid
-        x = self.linear3(x)
+        #x = self.linear3(x)
         y_pred = torch.sigmoid(x)
         return y_pred
 
@@ -66,10 +67,10 @@ def SplitData(data_path):
     training, validation, and test data.
     NB: The data is not really raw - the TalkBank data has been preprocessed - see xxx.
     """
-    # Import the dataframe
+    # # Import the dataframe
     data = pd.read_csv(data_path)
 
-    # Split dataset into train, test, val (70, 15, 15)
+    # # Split dataset into train, test, val (70, 15, 15)
     train, test = train_test_split(data, test_size=0.15)
     train, val = train_test_split(train, test_size=0.15)
 
@@ -77,9 +78,17 @@ def SplitData(data_path):
     train = Dataset.from_dict(train)
     val = Dataset.from_dict(val)
     test = Dataset.from_dict(test)
+
+    # from datasets import load_dataset
+
+    # dataset = load_dataset("rotten_tomatoes")
+
+    # obtain train, val and test splits
+    # train = dataset["train"]
+    # val = dataset["validation"]
+    # test = dataset["test"]
     
     return train, val, test
-
 
 ###### LOADING THE DATA ######
 def Vectorize(data_path): # xxx rename to TF_IDF() or BOW() one you have decided whether to use one or the other vectorizer.
@@ -91,49 +100,50 @@ def Vectorize(data_path): # xxx rename to TF_IDF() or BOW() one you have decided
     train, val, test = SplitData(data_path)
 
     # Define vectorizer
-    vectorizer = TfidfVectorizer(ngram_range=(1,2),
-                            #lowercase=True, 
+    vectorizer = TfidfVectorizer(ngram_range=(1,2), # xxx default is 1,1 which means that all we want from the vocab is for it to consist of 1 word tokens and 2 word tokens. e.g. good but also not good
+    # try to comment out the ngram and just use the default. Experiment with this.
+                            lowercase=True, 
                             #stop_words="english", 
-                            #max_df=0.9, 
-                            #min_df=0.1,
-                            max_features=500)
+                            #max_df=0.95, 
+                            #min_df=0.05,
+                            max_features=500) #reduce noise in the data
     
     
     # Vectorizing the datasets
-    train_vect = vectorizer.fit_transform(train["text"])
-    val_vect = vectorizer.transform(val["text"])
-    test_vect = vectorizer.transform(test["text"])
+    x_train = vectorizer.fit_transform(train["text"])
+    x_val = vectorizer.transform(val["text"])
+    x_test = vectorizer.transform(test["text"])
 
     # Turning the vectorized data into tensors
     ## Training data:
-    train_vect = torch.tensor(train_vect.toarray(), dtype=torch.float)
-    train_label = torch.tensor(list(train["label"]), dtype=torch.float)
-    train_label = train_label.view(train_label.shape[0], 1)
+    x_train = torch.tensor(x_train.toarray(), dtype=torch.float)
+    y_train = torch.tensor(list(train["label"]), dtype=torch.float)
+    y_train = y_train.view(y_train.shape[0], 1)
 
     # Validation data:
-    val_vect = torch.tensor(val_vect.toarray(), dtype=torch.float)
-    val_label = torch.tensor(list(val["label"]), dtype=torch.float)
-    val_label = val_label.view(val_label.shape[0], 1)
+    x_val = torch.tensor(x_val.toarray(), dtype=torch.float)
+    y_val = torch.tensor(list(val["label"]), dtype=torch.float)
+    y_val = y_val.view(y_val.shape[0], 1)
 
     # Test data
-    test_vect = torch.tensor(test_vect.toarray(), dtype=torch.float)
-    test_label = torch.tensor(list(test["label"]), dtype=torch.float)
-    test_label = test_label.view(test_label.shape[0], 1)
+    x_test = torch.tensor(x_test.toarray(), dtype=torch.float)
+    y_test = torch.tensor(list(test["label"]), dtype=torch.float)
+    y_test = y_test.view(y_test.shape[0], 1)
 
-    return train_vect, train_label, val_vect, val_label, test_vect, test_label
+    return x_train, y_train, x_val, y_val, x_test, y_test
 
 
 
 ###### SET MODEL PARAMETERS ######
-def InitializeModel(train_vect, classifier, learning_rate):
+def InitializeModel(x_train, classifier, learning_rate):
 
-    n_samples, n_features = train_vect.shape
+    n_samples, n_features = x_train.shape
 
     # initializing the chosen classifier
     if classifier == 'nn':
         model = NeuralNetwork(n_input_features=n_features)
     
-    elif classifier == 'logreg':
+    elif classifier == 'lr':
         model = LogReg(n_input_features=n_features)
 
     else:
@@ -143,7 +153,7 @@ def InitializeModel(train_vect, classifier, learning_rate):
     # define loss and optimizer
     criterion = nn.BCELoss()
     optimizer = torch.optim.AdamW(model.parameters(),
-                                lr = learning_rate) # 1e-3
+                                lr = learning_rate)
     
     return n_features, criterion, optimizer, model
 
@@ -154,7 +164,7 @@ def Train(data_path, epochs, classifier, learning_rate): # xxx plot = True
     print("[INFO:] Training classifier...")
 
     # Initialize the model
-    n_features, criterion, optimizer, model = InitializeModel(train_vect, classifier, learning_rate)
+    n_features, criterion, optimizer, model = InitializeModel(x_train, classifier, learning_rate)
 
     # for plotting
     train_loss_history = []
@@ -164,10 +174,10 @@ def Train(data_path, epochs, classifier, learning_rate): # xxx plot = True
     for epoch in range(epochs):
     
         # forward
-        y_hat = model(train_vect)
+        y_hat = model(x_train)
 
         # backward
-        loss = criterion(y_hat, train_label)
+        loss = criterion(y_hat, y_train)
         train_loss_history.append(loss)
 
         # backpropagation
@@ -183,10 +193,10 @@ def Train(data_path, epochs, classifier, learning_rate): # xxx plot = True
             model.eval() 
 
             # make predictions
-            predicted_outputs = model(val_vect) 
+            y_val_hat = model(x_val) # predicted outputs
 
             # metrics
-            val_loss = criterion(predicted_outputs, val_label) 
+            val_loss = criterion(y_val_hat, y_val) 
 
             # append
             val_loss_history.append(val_loss) 
@@ -202,23 +212,27 @@ def Train(data_path, epochs, classifier, learning_rate): # xxx plot = True
     val_loss = [val.item() for val in val_loss_history]
 
     fig, ax = plt.subplots()
-    ax.plot(train_loss)
-    ax.plot(val_loss)
+    ax.plot(train_loss, label = 'train')
+    ax.set_title('Loss history')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss')
+    ax.plot(val_loss, label = 'val')
+    ax.legend()
     plt.savefig(os.path.join("/work", "exam", "ASD_classification", "out", "loss_curve_" + classifier + ".png"))
 
     return model, n_features
 
 
 ###### EVALUATING THE MODEL ######
-def Test(n_features, model, test_vect, test_label, classifier):
+def Test(n_features, model, x_test, y_test, classifier):
     """ Function to evaluate model on test set
     Args:
         n_features (int): number of features
     """
 
     # Write classification report
-    predicted = model(test_vect).detach().numpy()
-    output = classification_report(test_label, 
+    predicted = model(x_test).detach().numpy()
+    output = classification_report(y_test, 
                                 np.where(predicted > 0.5, 1, 0),
                                 target_names = ["TD", "ASD"])
 
@@ -236,19 +250,20 @@ if __name__ == "__main__":
 
     # Receiving user input
     classifier = input("It is time select your classifier! \
-    Type logreg for logistic regression or nn for a simple neural network \
+    Type lr for logistic regression or nn for a simple neural network \
     with one hidden layers containing 30 nodes: ")
     print(f'\n Your chosen classifier {classifier} will be fitted shortly')
+    # xxx make sure to revisit this text when you have decided on the size of the network
 
-    # User defined variables
-    data_path = "/work/exam/ASD_classification/data/dataframes/data_eigstig_age3_text_label.csv"
-    epochs = 1000
-    learning_rate = 1e-4
+    # # User defined variables
+    data_path = "/work/exam/ASD_classification/data/dataframes/data_eigstig_text_label.csv"
+    epochs = 200
+    learning_rate = 1e-3 # 1e-4 = 0.0001, 1e-3 = 0.001
 
     # Splitting into separate datasets
-    train_vect, train_label, val_vect, val_label, test_vect, test_label = Vectorize(data_path)
+    x_train, y_train, x_val, y_val, x_test, y_test = Vectorize(data_path)
 
     # training the model
     model, n_features = Train(data_path, epochs, classifier, learning_rate)
 
-    Test(n_features, model, test_vect, test_label, classifier)
+    Test(n_features, model, x_test, y_test, classifier)
